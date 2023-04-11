d46470af6a0368b6daae2df15ee5d8690ebbb0f6
Bin Wang <bwang28c@gmail.com>
2023-04-11 11:26:09 +0800

    new

--- jemdoc/compile.sh
+++ jemdoc/compile.sh
@@ -3,6 +3,8 @@
 
 python2 jemdoc.py -o ../ index.jemdoc
 
+python2 jemdoc.py -o ../ index_chinese.jemdoc
+
 python2 jemdoc.py -o ../ research.jemdoc
 
 python2 jemdoc.py -o ../ publications.jemdoc
@@ -12,5 +14,6 @@ python2 jemdoc.py -o ../ teaching.jemdoc
 python2 jemdoc.py -o ../ activities.jemdoc
 
 python2 jemdoc.py -o ../ biography.jemdoc
+
 
 #python2 jemdoc.py -o ../ statistics.jemdoc
